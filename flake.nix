1ad746980f8890fe76a762911bdb6e4940883d12
Stefan Bondzulic <stefanbondzulic@gmail.com>
2024-03-13 23:29:59 +0100

    feat: add bern server

--- flake.nix
+++ flake.nix
@@ -3,69 +3,68 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... } @inputs: {
-    nixosConfigurations = {
-      "dublin" = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [ ./nixos/dublin.nix ];
+  outputs = { nixpkgs, home-manager, nixpkgs-stable, ... }@inputs:
+    let
+      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+      pkgs-stable = nixpkgs-stable.legacyPackages.aarch64-darwin;
+    in {
+      nixosConfigurations = {
+        "bern" = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [ ./nixos/bern.nix ];
+        };
+
+        "dublin" = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [ ./nixos/dublin.nix ];
+        };
+      };
+
+      homeConfigurations = {
+        "stefanbondzulic@hydrogen" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+          extraSpecialArgs = { inherit inputs; };
+          modules = [ ./home-manager/hydrogen.nix ];
+          extraSpecialArgs = { inherit pkgs-stable; };
+        };
+
+        "bondzula@bondzula" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          extraSpecialArgs = { inherit inputs; };
+          modules = [ ./home-manager/bondzula.nix ];
+        };
+
+        # Servers
+        "bondzula@rome" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          extraSpecialArgs = { inherit inputs; };
+          modules = [ ./home-manager/rome.nix ];
+        };
+
+        "bondzula@bern" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          extraSpecialArgs = { inherit inputs; };
+          modules = [ ./home-manager/bern.nix ];
+        };
+
+        "bondzula@helsinki" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          extraSpecialArgs = { inherit inputs; };
+          modules = [ ./home-manager/helsinki.nix ];
+        };
+
+        "bondzula@oslo" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          extraSpecialArgs = { inherit inputs; };
+          modules = [ ./home-manager/oslo.nix ];
+        };
       };
     };
-
-    homeConfigurations = {
-      "stefanbondzulic@hydrogen" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-        extraSpecialArgs = { inherit inputs; };
-        modules = [
-          ./home-manager/hydrogen.nix
-        ];
-      };
-
-      "bondzula@bondzula" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        extraSpecialArgs = { inherit inputs; };
-        modules = [
-          ./home-manager/bondzula.nix
-        ];
-      };
-
-      # Servers
-      "bondzula@rome" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        extraSpecialArgs = { inherit inputs; };
-        modules = [
-          ./home-manager/rome.nix
-        ];
-      };
-
-      "bondzula@bern" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        extraSpecialArgs = { inherit inputs; };
-        modules = [
-          ./home-manager/bern.nix
-        ];
-      };
-
-      "bondzula@helsinki" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        extraSpecialArgs = { inherit inputs; };
-        modules = [
-          ./home-manager/helsinki.nix
-        ];
-      };
-      
-      "bondzula@oslo" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        extraSpecialArgs = { inherit inputs; };
-        modules = [
-          ./home-manager/oslo.nix
-        ];
-      };
-    };
-  };
 }
---
+++ nixos/bern.nix
@@ -0,0 +1,31 @@
+{ pkgs, modulesPath, ... }:
+
+{
+  imports = [ (modulesPath + "/virtualisation/lxc-container.nix") ];
+
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  environment.systemPackages = with pkgs; [ neovim git ];
+
+  networking.hostName = "bern";
+
+  programs.zsh.enable = true;
+
+  users.users = {
+    bondzula = {
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIa8ipSFMoDgS8u+AVtxeokw5OB4DvF86GFcZAzJ74Lk stefanbondzulic@gmail.com"
+      ];
+      extraGroups = [ "wheel" ];
+      shell = pkgs.zsh;
+    };
+  };
+
+  system.stateVersion = "23.11";
+}
